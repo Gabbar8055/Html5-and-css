0c9e187c26f768457fc05e8dfeed208c9a839f92
Mayank Kuthar <43777451+Kuthar8055@users.noreply.github.com>
2019-10-02 15:29:23 +0530

    Create adastair.py

---
+++ adastair.py
@@ -0,0 +1,18 @@
+x=int(input())
+for p in range(x):
+    n, k=map(int, input().split())
+    h=list(map(int, input().split()))
+    c=0
+    if h[0]>k:
+    	if h[0]%k==0:
+    		c=c+h[0]//k-1
+    	else:
+    		c=c+h[0]//k
+    for j in range(1, n):
+        if h[j]-h[j-1]<=k:
+            continue
+        elif (h[j]-h[j-1])%k==0:
+            c=c+(h[j]-h[j-1])//k-1
+        else:
+            c=c+(h[j]-h[j-1])//k
+    print(c)
